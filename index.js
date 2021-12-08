1c7d136e3794cc0aec46bdcd4cbedb432bd9c26b
MiGu3L <MiGu3L_4nG3L@outlook.com>
2021-12-08 16:30:14 -0600

    responsive, lista de ususarios dinamica y mongodb

--- index.js
+++ index.js
@@ -2,22 +2,51 @@ const express = require('express');
 const app = express();
 const path = require("path")
 const http = require('http');
+const mongoose = require('mongoose')
+const Message = require('./public/models/message')
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+let users_list = []
 
 app.set("port", process.env.PORT || 3000);
 app.use(express.static(path.join(__dirname, "public")));
+
+mongoose.connect('mongodb+srv://siproject:siproject@siproject.g8owq.mongodb.net/chat_db?retryWrites=true&w=majority')
+  .then(db => console.log('db connected'))
+  .catch(err => console.log(err))
 
 io.on('connection', (socket) => {
     console.log('a user connected');
 
     socket.on('disconnect', () => {
-      console.log('user disconnected');
+      if (!socket.user) return
+      users_list.splice(users_list.indexOf(socket.user), 1)
+      io.sockets.emit('users:list', users_list)
     });
 
-    socket.on('chat:message', (data) => {
+    socket.on('chat:message', async (data) => {
+        await new Message({
+          user: socket.user,
+          msg: data.message
+        }).save()
+
         io.sockets.emit('res:message', data)
+    })
+
+    socket.on('new:user_conn', async (data, cb)=>{
+      if(users_list.indexOf(data) != -1){
+        cb(false)
+      }else{
+        cb(true)
+        socket.user = data
+        users_list.push(socket.user)
+        io.sockets.emit('users:list', users_list)
+
+        let old_messages = await Message.find()
+        console.log(old_messages);
+        socket.emit('old:messages', old_messages)
+      }
     })
   });
 
--- public/script.js
+++ public/script.js
@@ -6,15 +6,25 @@ window.addEventListener("DOMContentLoaded", () => {
   let username = document.querySelector('#username')
   let content = document.querySelector('.content')
   let h6 = document.querySelector('#user')
+  let panel_left = document.querySelector('.panel-left')
+  let friends = document.querySelector('.friends')
   let active_user
 
+  document.querySelector('.open-users').onclick = function (event) {
+    panel_left.classList.toggle('panel-mobile');
+  }
+
   document.querySelector('#ingresar').onclick = function (event) {
-      if(username.value){
-        active_user = username.value
-        h6.innerHTML = username.value
-        login.style.display = 'none'
-        content.style.display = 'block'
-      }
+      socket.emit('new:user_conn', username.value, (res)=>{
+        if(res){
+          active_user = username.value
+          h6.innerHTML = username.value
+          login.style.display = 'none'
+          content.style.display = 'block'
+        }else{
+          console.log("el usuario ya existe!");
+        }
+      });
   }
 
   document.querySelector('#send').onclick = function (event) {
@@ -53,6 +63,54 @@ window.addEventListener("DOMContentLoaded", () => {
                             `
     }
 
+  })
+
+  socket.on('users:list', (res)=>{
+    let list = ''
+    for (i in res){
+      if (res[i] != active_user) {
+        list += `<div class="friend-drawer friend-drawer--onhover">
+                  <img class="profile-image" src="assets/images/avatar.jpg" alt="">
+                  <div class="text">
+                    <h6>${res[i]}</h6>
+                    <p class="text-m">Disponible</p>
+                  </div>
+                    <span class="text-m small">13:21</span>
+                  </div>
+                <hr>`
+      }
+    }
+
+    friends.innerHTML = list
+  })
+
+  socket.on('old:messages', (data)=>{
+    console.log(data[0].user, active_user)
+    for(i in data){
+      if (data[i].user == active_user) {
+        chat_panel.innerHTML += `
+                              <div class="row g-0">
+                                <div class="col-12">
+                                  <div class="chat-bubble chat-bubble--right text-end">
+                                    <span class="message-autor-right">${data[i].user}</span>
+                                    ${data[i].msg}
+                                  </div>
+                                </div>
+                              </div>
+                            `
+      } else {
+        chat_panel.innerHTML += `
+                                <div class="row g-0">
+                                  <div class="col-12">
+                                    <div class="chat-bubble chat-bubble--left text-start">
+                                    <span class="message-autor-left">${data[i].user}</span>
+                                    ${data[i].msg}
+                                    </div>
+                                  </div>
+                                </div>
+                              `
+      }
+    }
   })
 })
 
