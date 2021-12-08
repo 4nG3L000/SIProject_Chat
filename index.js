d2b6a30dc3885c645b781f7896a3615746d654e4
MiGu3L <MiGu3L_4nG3L@outlook.com>
2021-12-07 19:47:15 -0600

    habilitado enviar y recibir mensajes

--- index.js
+++ index.js
@@ -6,12 +6,21 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+app.set("port", process.env.PORT || 3000);
 app.use(express.static(path.join(__dirname, "public")));
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
-});
+    console.log('a user connected');
 
-server.listen(3000, () => {
-  console.log('listening on *:3000');
-});
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
+
+    socket.on('chat:message', (data) => {
+        io.sockets.emit('res:message', data)
+    })
+  });
+
+
+server.listen(app.get("port"));
+console.log(`servidor iniciado:${app.get("port")}`);
--- public/script.js
+++ public/script.js
@@ -1,1 +1,58 @@
-io()
+window.addEventListener("DOMContentLoaded", () => {
+  let socket = io()
+  let new_message = document.getElementById('message')
+  let chat_panel = document.querySelector('.chat-panel')
+  let login = document.querySelector('.login')
+  let username = document.querySelector('#username')
+  let content = document.querySelector('.content')
+  let h6 = document.querySelector('#user')
+  let active_user
+
+  document.querySelector('#ingresar').onclick = function (event) {
+      if(username.value){
+        active_user = username.value
+        h6.innerHTML = username.value
+        login.style.display = 'none'
+        content.style.display = 'block'
+      }
+  }
+
+  document.querySelector('#send').onclick = function (event) {
+    if (new_message.value) {
+      data = {
+        message: new_message.value,
+        username: active_user
+      }
+      socket.emit('chat:message', data);
+      new_message.value = '';
+    }
+  }
+
+  socket.on('res:message', (res)=>{
+    if (res.username == active_user) {
+      chat_panel.innerHTML += `
+                            <div class="row g-0">
+                              <div class="col-12">
+                                <div class="chat-bubble chat-bubble--right text-end">
+                                  <span class="message-autor-right">${res.username}</span>
+                                  ${res.message}
+                                </div>
+                              </div>
+                            </div>
+                          `
+    } else {
+      chat_panel.innerHTML += `
+                              <div class="row g-0">
+                                <div class="col-12">
+                                  <div class="chat-bubble chat-bubble--left text-start">
+                                  <span class="message-autor-left">${res.username}</span>
+                                  ${res.message}
+                                  </div>
+                                </div>
+                              </div>
+                            `
+    }
+
+  })
+})
+
